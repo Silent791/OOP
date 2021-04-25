26a479f889562d60307521a62424735631e4a3b6
Silent791 <72013009+Silent791@users.noreply.github.com>
2021-04-26 00:48:33 +0300

    Add files via upload

---
+++ 3 лаба/src/com/company/Location.java
@@ -0,0 +1,40 @@
+package com.company;
+
+public class Location {
+    /** X coordinate of this location. **/
+    public int xCoord;
+
+    /** Y coordinate of this location. **/
+    public int yCoord;
+
+
+    /** Creates a new location with the specified integer coordinates. **/
+    public Location(int x, int y)
+    {
+        xCoord = x;
+        yCoord = y;
+    }
+
+    /** Creates a new location with coordinates (0, 0). **/
+    public Location()
+    {
+        this(0, 0);
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this)
+            return true;
+        if (obj == null || obj.getClass() != this.getClass())
+            return false;
+        Location location = (Location) obj;
+        return xCoord == location.xCoord && yCoord == location.yCoord;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + xCoord;
+        result = 31 * result + yCoord;
+        return result;
+    }
+}
